b1473349a35eaa5dd8da565599638fc03cbcb64f
Bramuel Aswani~ <aswanbramuel@gmail.com>
2022-06-04 14:02:35 +0300

    plane 3D scatter

---
+++ 3D_plane_scatter.py
@@ -0,0 +1,31 @@
+# Create 3D scatter plot
+fig = px.scatter_3d(
+    df,
+    x="lon",
+    y="lat",
+    z="price_aprox_usd",
+    labels={"lon": "longitude", "lat": "latitude", "price_aprox_usd": "price"},
+    width=600,
+    height=500,
+)
+
+# Create x and y coordinates for model representation
+x_plane = np.linspace(df["lon"].min(), df["lon"].max(), 10)
+y_plane = np.linspace(df["lat"].min(), df["lat"].max(), 10)
+xx, yy = np.meshgrid(x_plane, y_plane)
+
+# Use model to predict z coordinates
+z_plane = model.predict(pd.DataFrame({"lon": x_plane, "lat": y_plane}))
+zz = np.tile(z_plane, (10, 1))
+
+# Add plane to figure
+fig.add_trace(go.Surface(x=xx, y=yy, z=zz))
+
+# Refine formatting
+fig.update_traces(
+    marker={"size": 4, "line": {"width": 2, "color": "DarkSlateGrey"}},
+    selector={"mode": "markers"},
+)
+
+# Display figure
+fig.show()
